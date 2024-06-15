99ad870bcb927bad692fbc788fcd44e61957d507
Santhosha-B-K <santhubtpura@gmail.com>
2024-06-15 12:17:09 +0530

    changed

---
+++ abcd.js
@@ -0,0 +1,8 @@
+// let a=document.getElementById("option")
+// // a.style.backgrounColor="red";
+// a.onclick=()=>{
+//     console.log("abcd")
+// }
+//  a.addEventListener("onclick" ()=>{
+// console.log("hhh")
+//  });
---
+++ day7.js
@@ -0,0 +1,18 @@
+// localStorage.setItem('user','abc');
+// const user={
+//     username:"xyz"
+// };
+// localStorage.setItem("user",JSON.stringify(user));
+// let u=localStorage.getItem("username")
+
+
+function abc1(){
+
+let ab=document.getElementById("a");
+ab.onclick(()=>
+{
+    document.body.style.backgroundColor="black";
+})
+// document.body.style.backgroundColor="black";
+// document.getElementsByTagName("h1").style.color="white"
+}
